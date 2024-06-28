a71c07433cdf3a6a975c56dd99b0b2aab6fee931
choiseongho <choiseongho@unfine.co.kr>
2024-06-28 09:36:03 +0900

    1

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 
 module.exports = {
   devServer: {
-    host: '0.0.0.0'
+    host: "0.0.0.0",
     port: 3000, // 원하는 포트 번호로 변경
   },
   lintOnSave: false, // 저장 시 ESLint를 비활성화합니다.
