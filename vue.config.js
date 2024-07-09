590fc9a24747436a992d7ac83bc15be807e50a17
choiseongho <choiseongho@unfine.co.kr>
2024-07-09 17:24:18 +0900

    env 파일 수정

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,14 @@ module.exports = defineConfig({
 module.exports = {
   devServer: {
     host: "0.0.0.0",
-    port: 3000, // 원하는 포트 번호로 변경
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://43.202.24.139:8080",
+        changeOrigin: true,
+        pathRewrite: { "^/api": "/api" }, // '/api' 경로를 유지
+      },
+    },
   },
   lintOnSave: false, // 저장 시 ESLint를 비활성화합니다.
 };
