01b9c045f72f6071b4b4055052a8c35750b3485c
choiseongho <choiseongho@unfine.co.kr>
2024-07-09 17:48:53 +0900

    proxy 테스트

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,7 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://43.202.24.139:8080",
-        changeOrigin: true,
-        pathRewrite: { "^/api": "/api" }, // '/api' 경로를 유지
-      },
-    },
+    proxy: process.env.VUE_APP_BASE_API, // proxy: process.env.VUE_APP_BASE_API //
   },
   lintOnSave: false, // 저장 시 ESLint를 비활성화합니다.
 };
